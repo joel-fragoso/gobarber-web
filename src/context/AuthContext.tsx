308de0cfedb2bfc174b715bed5929c46328e4525
Joel Fragoso <joelfragoso85@icloud.com>
2020-07-02 13:39:10 -0300

    feat: context sign in

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,33 @@
+import React, { createContext, FunctionComponent, useCallback } from 'react';
+import api from '../services/api';
+
+interface SignInCredential {
+  email: string;
+  password: string;
+}
+
+interface AuthContextData {
+  name: string;
+  signIn(credentials: SignInCredential): Promise<void>;
+}
+
+export const AuthContext = createContext<AuthContextData>(
+  {} as AuthContextData,
+);
+
+export const AuthProvider: FunctionComponent = ({ children }) => {
+  const signIn = useCallback(async ({ email, password }) => {
+    const response = await api.post('/sessions', {
+      email,
+      password,
+    });
+
+    console.log(response.data);
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ name: 'Joel Fragoso', signIn }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};

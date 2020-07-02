157b027cce32d4e8018166156e0ebdbb570ed0fb
Joel Fragoso <joelfragoso85@icloud.com>
2020-07-02 15:53:57 -0300

    feat: toasts structure

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -45,9 +45,11 @@ const SignIn: FunctionComponent = () => {
           password: data.password,
         });
       } catch (err) {
-        const errors = getValidationErrors(err);
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
 
-        formRef.current?.setErrors(errors);
+          formRef.current?.setErrors(errors);
+        }
       }
     },
     [signIn],

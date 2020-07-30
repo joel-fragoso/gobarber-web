9a97c0e82b55c9e44c27abbcf148d9ea5d74874c
Joel Fragoso <joelfragoso85@icloud.com>
2020-07-30 16:30:55 -0300

    feat: add forgot password

--- src/components/ToastContainer/Toast/index.tsx
+++ src/components/ToastContainer/Toast/index.tsx
@@ -33,7 +33,7 @@ const Toast: FunctionComponent<ToastProps> = ({ message, style }) => {
   return (
     <Container
       type={message.type}
-      hasDescription={!!message.description}
+      hasdescription={Number(!!message.description)}
       style={style}
     >
       {icons[message.type || 'info']}
